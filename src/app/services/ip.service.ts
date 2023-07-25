69fd45b65bc2c9a48f1bb261bd2d0e2ae597c948
ach02raf <ach02raf@gmail.com>
2023-07-25 13:38:38 +0100

    7-25-2023

--- src/app/services/ip.service.ts
+++ src/app/services/ip.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class IpService {
   //  192.168.30.253 192.168.1.12
   //  public ip = '192.168.20.243';
-  public ip = '192.168.1.3';
+  public ip = '192.168.1.14';
   // public ip = 'localhost';
   constructor() {}
 }
